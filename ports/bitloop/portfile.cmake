7bd01b15f4caf8e8f0f9e71e773829f9d66c5795
Will <whemsworth@hotmail.co.uk>
2025-07-30 18:22:21 +0100

    wip

--- ports/bitloop/portfile.cmake
+++ ports/bitloop/portfile.cmake
@@ -6,7 +6,7 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH  SOURCE_PATH
     REPO             willmh93/bitloop
-    REF              v${PORT_VERSION_STRING}
+    REF              v${VERSION}
     SHA512           61c7955c648016c39777ee6f6294e25f9d5345564291dd0121801510987147121af92c9c57f51e0e87e20dfa3f67be2558d26d1d2f35757f8df2262a1432a80e
 )
 
