aab612cc03010cb2cd06b94f137b9ff4c587dffd
Will <whemsworth@hotmail.co.uk>
2025-09-01 21:47:09 +0100

    bitloop new version

--- ports/bitloop/portfile.cmake
+++ ports/bitloop/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH  SOURCE_PATH
     REPO             willmh93/bitloop
     REF              v${VERSION}
-    SHA512           575fdbb4740d674b4b5e976551c1c82bb2cdae2199e7899be361571c52adb733d8de3187177e31d481137d94325902ee254113086d64479fb1faeb6e51ad0687
+    SHA512           64c7faf5b428dd1199137a3b566c1b19a141e1c8479166a3445c0a629e5507861c2f9b974324a0679697bda1119e5a8306fc40d816c796c0edc389101ea3f73c
 )
 
 # 3. Configure your Bitloop
