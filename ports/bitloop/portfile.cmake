b3778fad74058e42aaba6007e19e44ec6f3e677f
Will <whemsworth@hotmail.co.uk>
2025-10-22 00:07:49 +0100

    portfile feature support for cmake, simpler feature names

--- ports/bitloop/portfile.cmake
+++ ports/bitloop/portfile.cmake
@@ -8,12 +8,20 @@ vcpkg_from_github(
     SHA512           e7d6c8a49fc501e0a32926f37639f56ff5dd5e71571ed1123f564acc72bc244d975367098c2006c0f9a6b692acdbead0c315aee36fce164ca09343ba824fb6f4
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTS
+    FEATURES
+        ffmpeg        BITLOOP_WITH_FFMPEG
+        ffmpeg-x265   BITLOOP_WITH_FFMPEG BITLOOP_WITH_FFMPEG_X265
+)
+
+
 # 3. Configure your Bitloop
 vcpkg_configure_cmake(
   SOURCE_PATH    "${SOURCE_PATH}"
   PREFER_NINJA
   OPTIONS
     "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
+    ${FEATURE_OPTS}
 )
 
 # 4. Install it into vcpkg’s staging area
