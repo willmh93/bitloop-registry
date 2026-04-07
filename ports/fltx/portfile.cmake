088862a257c34b21193a5d4cafb7bdb33d6d6f96
Will <whemsworth@hotmail.co.uk>
2026-04-07 04:19:02 +0100

    add fltx port

---
+++ ports/fltx/portfile.cmake
@@ -0,0 +1,11 @@
+{
+  "name": "fltx",
+  "version-string": "0.1.0",
+  "description": "High-precision double-double and quad-double floating-point types and math utilities",
+  "homepage": "https://github.com/<you>/fltx",
+  "license": "MIT",
+  "dependencies": [
+    { "name": "vcpkg-cmake", "host": true },
+    { "name": "vcpkg-cmake-config", "host": true }
+  ]
+}
