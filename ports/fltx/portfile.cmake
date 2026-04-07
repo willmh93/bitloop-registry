55385edf1885b76dc139b0778e43cdfc21f9c670
Will <whemsworth@hotmail.co.uk>
2026-04-07 14:40:35 +0100

    fix broken portfile

--- ports/fltx/portfile.cmake
+++ ports/fltx/portfile.cmake
@@ -1,11 +1,22 @@
-{
-  "name": "fltx",
-  "version-string": "0.1.0",
-  "description": "High-precision double-double and quad-double floating-point types and math utilities",
-  "homepage": "https://github.com/<you>/fltx",
-  "license": "MIT",
-  "dependencies": [
-    { "name": "vcpkg-cmake", "host": true },
-    { "name": "vcpkg-cmake-config", "host": true }
-  ]
-}
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO willmh93/fltx
+    REF "v${VERSION}"
+    SHA512 0
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME fltx
+    CONFIG_PATH lib/cmake/fltx
+)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
